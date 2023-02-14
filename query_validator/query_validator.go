88d7d2c32eec71cca2360c299b0ba9c964a5e635
Andrii Sokyrko <wartwvister@gmail.com>
2023-02-14 11:25:37 +0100

    MAMORU-141: Add AssemblyScript validation

--- query_validator/query_validator.go
+++ query_validator/query_validator.go
@@ -1,7 +1,10 @@
 package query_validator
 
+/*
+#include <stdlib.h>
+*/
+import "C"
 import (
-	"C"
 	"errors"
 	"github.com/Mamoru-Foundation/query-validator-go/generated_bindings"
 	"unsafe"
@@ -14,12 +17,28 @@ const (
 	ChainEvm Chain = generated_bindings.FfiChainTypeEvm
 )
 
-func Validate(chain Chain, query string) error {
-	result := generated_bindings.FfiValidate(generated_bindings.FfiChainTypeT(chain), query)
+func ValidateSql(chain Chain, query string) error {
+	result := generated_bindings.FfiValidateSql(generated_bindings.FfiChainTypeT(chain), query)
 	defer generated_bindings.FfiDropValidationResult(result)
 
 	result.Deref()
 
+	return makeError(result)
+}
+
+func ValidateAssemblyScript(chain Chain, bytes []byte) error {
+	cBytes := sliceToFfi(bytes)
+	defer freeFfiSlice(cBytes)
+
+	result := generated_bindings.FfiValidateAssemblyScript(generated_bindings.FfiChainTypeT(chain), cBytes)
+	defer generated_bindings.FfiDropValidationResult(result)
+
+	result.Deref()
+
+	return makeError(result)
+}
+
+func makeError(result generated_bindings.FfiValidationResultT) error {
 	if result.IsError {
 		message := C.GoString((*C.char)(unsafe.Pointer(result.Message)))
 
@@ -27,4 +46,18 @@ func Validate(chain Chain, query string) error {
 	} else {
 		return nil
 	}
+}
+
+func sliceToFfi(bytes []byte) generated_bindings.SliceRefUint8T {
+	ptr := C.CBytes(bytes)
+
+	return generated_bindings.SliceRefUint8T{
+		Ptr: (*byte)(ptr),
+		Len: uint64(len(bytes)),
+	}
+}
+
+func freeFfiSlice(slice generated_bindings.SliceRefUint8T) {
+	C.free(unsafe.Pointer(slice.Ptr))
+	slice.Free()
 }
